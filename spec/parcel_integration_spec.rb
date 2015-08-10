dd8835d1db7316c3ed79bdd961ed8464ad79ecb8
Alexa Zeazas & Jennifer Bell <student@epicodus.com>
2015-08-10 14:18:51 -0700

    Add integration specs.

---
+++ spec/parcel_integration_spec.rb
@@ -0,0 +1,19 @@
+require('capybara/rspec')
+require('launchy')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('the parcel path', {:type => :feature}) do
+  it('processes the user entry and returns the total shipping cost') do
+    visit('/')
+    fill_in('length', :with => 2)
+    fill_in('width', :with => 2)
+    fill_in('depth', :with => 2)
+    fill_in('weight', :with => 2)
+    fill_in('distance', :with => 100)
+    fill_in('speed', :with => 5)
+    click_button('CALCULATE COST')
+    expect(page).to have_content('30')
+  end
+end
