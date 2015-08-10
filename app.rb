ff716288f34d240517a3c0ea61cc39ff35401528
Alexa Zeazas & Jennifer Bell <student@epicodus.com>
2015-08-10 14:45:14 -0700

    Add variables to /cost path in app.rb file.

--- app.rb
+++ app.rb
@@ -8,6 +8,12 @@ get('/') do
 end
 
 get('/cost') do
+  @length = params.fetch('length').to_i
+  @width = params.fetch('width').to_i
+  @depth = params.fetch('depth').to_i
+  @weight = params.fetch('weight').to_i
+  @speed = params.fetch('speed').to_i
+  @distance = params.fetch('distance').to_i
   @cost = Parcel.new(@length, @width, @depth, @weight).cost_to_ship(@speed, @distance)
   erb(:cost)
 end
