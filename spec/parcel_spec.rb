cc758243ab48a665ba147dc906c9087a9f7dc079
Alexa Zeazas & Jennifer Bell <student@epicodus.com>
2015-08-10 14:10:05 -0700

    Correct expected outcome.

--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -3,13 +3,13 @@ require('parcel')
 
 describe(Parcel) do
   describe("#cost_to_ship") do
-    it("returns the volume of a given parcel") do
-      test_parcel = Parcel.new(2, 2, 2, 2)
-      expect(test_parcel.cost_to_ship()).to(eq(8))
-    end
+    # it("returns the volume of a given parcel") do
+    #   test_parcel = Parcel.new(2, 2, 2, 2)
+    #   expect(test_parcel.cost_to_ship()).to(eq(8))
+    # end
     it("returns the cost of shipping based on speed, distance, volume and weight") do
       test_parcel = Parcel.new(2, 2, 2, 2)
-      expect(test_parcel.cost_to_ship(100, 5)).to(eq(75))
+      expect(test_parcel.cost_to_ship(100, 5)).to(eq(30))
     end
   end
 end
